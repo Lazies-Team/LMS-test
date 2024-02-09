0f5fb980813473f74dbc2ab2184d9aaa891581b5
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-02-10 00:03:03 +0500

    Deadline edit property

--- src/Domain/Entities/Homework.cs
+++ src/Domain/Entities/Homework.cs
@@ -8,7 +8,7 @@ namespace Domain.Entities
         public string Description { get; set; }
         public long LessonId { get; set; }
         public long UserId { get; set; }
-        public DateTime Dedline { get; set; }
+        public DateTime Deadline { get; set; }
 
         public Lesson Lesson { get; set; }
         public User User { get; set; }
