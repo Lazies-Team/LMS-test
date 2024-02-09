be9b18fab829463e885f8f7a2f4f57630f437916
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-02-09 21:47:12 +0500

    Lesson Entities created

---
+++ src/Domain/Entities/Lesson.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Comman;
+
+namespace Domain.Entities
+{
+    public class Lesson : Auditable
+    {
+        public string Name { get; set; }
+        public DateOnly Date { get; set; }
+        //public Course CourseId { get; set; }
+
+    }
+}
