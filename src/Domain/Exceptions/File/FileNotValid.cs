62935182fd62ac761e45e3622887e3fe6e63e4bb
berdikulov-571 <mygithub571@gmail.com>
2024-02-10 01:13:54 +0500

    Exception

---
+++ src/Domain/Exceptions/File/FileNotValid.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Exceptions.File
+{
+    public class FileNotValid : GlobalException
+    {
+        public FileNotValid()
+        {
+            StatusCode = System.Net.HttpStatusCode.BadRequest;
+            TitleMessage = "File Format Not Valid !";
+        }
+    }
+}
