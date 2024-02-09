01770ee9c05ebb4f1094dbed278b4851953cab40
berdikulov-571 <mygithub571@gmail.com>
2024-02-10 01:13:51 +0500

    Exception

---
+++ src/Domain/Exceptions/File/FileNotFound.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Exceptions.File
+{
+    public class FileNotFound : GlobalException
+    {
+        public FileNotFound()
+        {
+            StatusCode = System.Net.HttpStatusCode.BadRequest;
+            TitleMessage = "File Not Found !";
+        }
+    }
+}
