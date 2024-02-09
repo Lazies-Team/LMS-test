c09b112b6303e586ef5a499cbd0471ebffe81589
berdikulov-571 <mygithub571@gmail.com>
2024-02-10 01:02:01 +0500

    Exception

---
+++ src/Domain/Exceptions/GlobalException.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace Domain.Exceptions
+{
+    public class GlobalException : Exception
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public string TitleMessage { get; set; } = string.Empty;
+    }
+}
