4b0e6d9640f42b574a641e8c1f1b1a829eced215
berdikulov-571 <mygithub571@gmail.com>
2024-02-10 01:41:14 +0500

    PasswordAttribute

---
+++ src/Domain/Attributes/Password/PasswordAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace Domain.Attributes.Password
+{
+    [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+    public class PasswordAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+
+            string password = value.ToString();
+
+            if (Regex.IsMatch(password, @"^(?=.*[A-Za-z])(?=.*\d).{6,}$"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
