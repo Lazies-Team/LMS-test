7cbd0eebbb2764f7f75c754a2025384fad91ad4c
berdikulov-571 <mygithub571@gmail.com>
2024-02-10 01:13:35 +0500

    Exception

---
+++ src/Domain/Exceptions/Lesson/LessonNotFound.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Exceptions.Lesson
+{
+    public class LessonNotFound : GlobalException
+    {
+        public LessonNotFound()
+        {
+            StatusCode = System.Net.HttpStatusCode.NotFound;
+            TitleMessage = "Lesson Not Found !";
+        }
+    }
+}
