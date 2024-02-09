9c6cd6367ce640cef1c84ba693ef8f61aa20280a
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-02-09 22:28:25 +0500

    created user interface for repository🧑‍🍼

---
+++ src/Application/Abstractions/GenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace Application.Abstractions
+{
+    public interface GenericRepository<TEntity>
+    {
+        ValueTask<TEntity> InsertAsync(TEntity entity);
+        IQueryable<TEntity> SelectAll();
+        ValueTask<TEntity> SelectByIdAsync(long id);
+        ValueTask<TEntity> UpdateAsync(TEntity entity);
+        ValueTask<TEntity> DeleteAsync(TEntity entity);
+    }
+}
