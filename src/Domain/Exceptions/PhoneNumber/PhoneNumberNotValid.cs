328a50a50ee13084ff8789847d69091abebd2012
berdikulov-571 <mygithub571@gmail.com>
2024-02-10 01:01:49 +0500

    Exception

---
+++ src/Domain/Exceptions/PhoneNumber/PhoneNumberNotValid.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Exceptions.PhoneNumber
+{
+    public class PhoneNumberNotValid : GlobalException
+    {
+        public PhoneNumberNotValid()
+        {
+            StatusCode = System.Net.HttpStatusCode.BadRequest;
+            TitleMessage = "Phone Number Not Valid !";
+        }
+    }
+}
