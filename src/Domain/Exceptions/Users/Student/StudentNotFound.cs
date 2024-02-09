3a62de455e8629206c61e9d99770045e521bf899
berdikulov-571 <mygithub571@gmail.com>
2024-02-10 01:02:29 +0500

    Exception

---
+++ src/Domain/Exceptions/Users/Student/StudentNotFound.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Exceptions.Users.Student
+{
+    public class StudentNotFound : GlobalException
+    {
+        public StudentNotFound()
+        {
+            StatusCode = System.Net.HttpStatusCode.NotFound;
+            TitleMessage = "Student Not Found !";
+        }
+    }
+}
