b66401867b16c5d35e4744b6c1c8e5c00be54f98
berdikulov-571 <mygithub571@gmail.com>
2024-02-10 01:01:40 +0500

    Exception

---
+++ src/Domain/Exceptions/PhoneNumber/PhoneNumberAlreadyExists.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Exceptions.PhoneNumber
+{
+    public class PhoneNumberAlreadyExists : GlobalException
+    {
+        public PhoneNumberAlreadyExists()
+        {
+            StatusCode = System.Net.HttpStatusCode.Conflict;
+            TitleMessage = "Phone Number Already Exists !";
+        }
+    }
+}
