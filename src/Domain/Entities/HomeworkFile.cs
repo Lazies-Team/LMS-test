c7929063a9be24cfaec48ebd0e9d4fd5dc01c51a
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-02-09 23:56:20 +0500

    Created Entities TaskResult,TaskResultFile,Homework,HomeworkFile,video,Grade

---
+++ src/Domain/Entities/HomeworkFile.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Comman;
+
+namespace Domain.Entities
+{
+    public class HomeworkFile : Auditable
+    {
+        public long HomeworkId { get; set; }
+        public string Path { get; set; }
+
+    }
+}
--- src/Domain/Entities/Video.cs
+++ src/Domain/Entities/Video.cs
@@ -3,12 +3,14 @@ using Domain.Entities.Users;
 
 namespace Domain.Entities
 {
-    public class Homework : Auditable
+    public class Video : Auditable
     {
+        public string Path { get; set; }
+        public string Size { get; set; }
         public string Description { get; set; }
         public long LessonId { get; set; }
         public long UserId { get; set; }
-        public DateTime Dedline { get; set; }
+
         public Lesson Lesson { get; set; }
         public User User { get; set; }
     }
