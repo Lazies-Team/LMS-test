97f7dc1e3c61f92ca96fece426443f490b4934fc
berdikulov-571 <mygithub571@gmail.com>
2024-02-10 01:01:36 +0500

    Exception

---
+++ src/Domain/Exceptions/Email/EmailNotValid.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Exceptions.Email
+{
+    public class EmailNotValid : GlobalException
+    {
+        public EmailNotValid()
+        {
+            StatusCode = System.Net.HttpStatusCode.BadRequest;
+            TitleMessage = "Email Not Valid !";
+        }
+    }
+}
