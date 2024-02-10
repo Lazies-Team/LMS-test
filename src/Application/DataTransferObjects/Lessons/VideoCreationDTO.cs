50c78d17acf2ccf5d4810de873d90980b6bdea74
berdikulov-571 <mygithub571@gmail.com>
2024-02-11 00:11:59 +0500

    VideoCreationDTO!

---
+++ src/Application/DataTransferObjects/Lessons/VideoCreationDTO.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Application.DataTransferObjects.Lessons
+{
+    public record class VideoCreationDTO
+    (
+        IFormFile Path,
+        string Size,
+        string Description,
+        long LessonId,
+        long TeacherId
+    );
+}
