4f4154b1fb761c468ac804e8de81d3afb2dd8a58
Sarvarbek G'ulomjonov <123171146+murodovich@users.noreply.github.com>
2024-02-11 00:52:13 +0500

    Update SpecialtyModificationDTO.cs

--- src/Application/DataTransferObjects/Courses/SpecialtyModificationDTO.cs
+++ src/Application/DataTransferObjects/Courses/SpecialtyModificationDTO.cs
@@ -1,6 +1,6 @@
 ﻿namespace Application.DataTransferObjects.Courses
 {
-    public record class SpecialtyModificationDTO 
+    public recor SpecialtyModificationDTO 
     (
         long Id,
         string Name
