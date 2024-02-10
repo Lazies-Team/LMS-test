15cd6622db06f4f611ef46243de7c2d851ec53e6
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-02-10 13:49:39 +0500

    Lesson implements relations

---
+++ src/Domain/Entities/Lessons/Attendance.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Comman;
+using Domain.Entities.Users;
+using Domain.Enums;
+
+namespace Domain.Entities.Lessons
+{
+    public class Attendance : Auditable
+    {
+        public long LessonId { get; set; }
+        public long StudentId { get; set; }
+        public AttendanceStatus Status { get; set; }
+        public string Description { get; set; }
+
+        public Lesson Lesson { get; set; }
+        public Student Student { get; set; }
+    }
+}
