c645fd973c73489c2257484b7dacfe295a607651
SardorSohinazarov <sardorstudent0618gmail.com>
2024-02-11 00:55:00 +0500

    DbContext added

---
+++ src/Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+            => Database.Migrate();
+
+        public DbSet<User> Users { get; set; }
+    }
+}
