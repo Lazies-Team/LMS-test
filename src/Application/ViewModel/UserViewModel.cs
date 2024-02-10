272128e1e93cb75bed9e915ea8a942b27ab783f3
SardorSohinazarov <sardorstudent0618gmail.com>
2024-02-10 15:06:43 +0500

    ViewModel created

---
+++ src/Application/ViewModel/UserViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities.Users;
+using Domain.Enums;
+
+namespace Application.ViewModel
+{
+    public record UserViewModel(
+        long Id,
+        string FirstName,
+        string LastNam,
+        string PhoneNumber,
+        DateTime BirthDate,
+        string ProfilePhotoPath,
+        Gender Gender,
+        string Login,
+        Role Role,
+        DateTime CreatedAt,
+        DateTime UpdatedDate
+        );
+}
