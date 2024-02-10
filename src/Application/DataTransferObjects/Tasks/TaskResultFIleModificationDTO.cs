f4b6f7b9a80d79ba1ea82c5f598a9ae9de9bb466
berdikulov-571 <mygithub571@gmail.com>
2024-02-11 00:48:24 +0500

    TaskFileModificationDTO

---
+++ src/Application/DataTransferObjects/Tasks/TaskResultFIleModificationDTO.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Application.DataTransferObjects.Tasks
+{
+    public record class TaskResultFIleModificationDTo
+    (
+        long Id,
+        long TaskResultId,
+        IFormFile FilePath
+    );
+}
