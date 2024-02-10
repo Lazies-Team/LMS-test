c24dcfd85d026874971584a728e9c1fa01a23543
SardorSohinazarov <sardorstudent0618gmail.com>
2024-02-10 15:03:38 +0500

    UserService implemented

---
+++ src/Application/Services/Users/UserService.cs
@@ -0,0 +1,39 @@
+﻿using Application.Abstractions.Users;
+using Application.DataTransferObjects.Users;
+using Application.ViewModel;
+
+namespace Application.Services.Users
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+            => _userRepository = userRepository;
+
+        public ValueTask<UserViewModel> AddAsync(UserCreationDTO userCreationDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<UserViewModel> DeleteAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<IList<UserViewModel>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<UserViewModel> GetByIdAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<UserViewModel> UpdateAsync(UserModificationDTO userModificationDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
