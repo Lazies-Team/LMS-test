feb067b52a0a335c7a1588d3ed5d9bc8f7d00564
berdikulov-571 <mygithub571@gmail.com>
2024-02-11 00:49:51 +0500

    AdminCreationDTO

---
+++ src/Application/DataTransferObjects/Users/AdminCreationDTO.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Enums;
+using Microsoft.AspNetCore.Http;
+using System.Text.Json.Serialization;
+
+namespace Application.DataTransferObjects.Users
+{
+    public record class AdminCreationDTO
+    (
+        long UserId
+    );
+}
