02b55fb7a62a33c713c793eb586233cf7d1577c2
berdikulov-571 <mygithub571@gmail.com>
2024-02-11 00:10:54 +0500

    AttandanceCreationDTO

---
+++ src/Application/DataTransferObjects/Lessons/AttandanceCreationDTO.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Enums;
+
+namespace Application.DataTransferObjects.Lessons
+{
+    public record class AttandanceCreationDTO
+    (
+        long LessonId,
+        long StudentId,
+        AttendanceStatus Status,
+        string Description
+    );
+}
