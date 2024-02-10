718a2bf53fb2eb79b850b24626aca209c7270e1f
SardorSohinazarov <sardorstudent0618gmail.com>
2024-02-10 15:03:24 +0500

    UserDTOs implemented

---
+++ src/Application/DataTransferObjects/Users/UserModificationDTO.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Enums;
+using Microsoft.AspNetCore.Http;
+
+namespace Application.DataTransferObjects.Users
+{
+    public record UserModificationDTO(
+        string FirstName,
+        string LastName,
+        string PhoneNumber,
+        DateTime BirthDate,
+        IFormFile ProfilePhotoPath,
+        Gender Gender,
+        string Login,
+        string Password,
+        long RoleId
+        );
+}
