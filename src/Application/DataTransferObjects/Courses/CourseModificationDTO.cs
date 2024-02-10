42e8a5909f3d6c6425cc896dd6dbbee108da9272
berdikulov-571 <mygithub571@gmail.com>
2024-02-11 00:10:22 +0500

    CourseModificationDTO

---
+++ src/Application/DataTransferObjects/Courses/CourseModificationDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace Application.DataTransferObjects.Courses
+{
+    public record class CourseModificationDTO
+    (
+        long Id,
+        string Name,
+        long SpecialityId,
+        TimeOnly LessonStartAt,
+        TimeOnly LessonEndAt,
+        DateOnly StartAt,
+        DateOnly EndAt
+    );
+}
