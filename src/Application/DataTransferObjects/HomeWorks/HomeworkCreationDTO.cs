c6d04041b4e748c291850bd26f29a98b3d25289d
berdikulov-571 <mygithub571@gmail.com>
2024-02-11 00:11:08 +0500

    HomeWorkCreationDTO

---
+++ src/Application/DataTransferObjects/HomeWorks/HomeworkCreationDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.DataTransferObjects.HomeWorks
+{
+    public record class HomeworkCreationDTO
+    (
+        string Description,
+        long LessonId,
+        DateTime Deadline
+    );
+}
