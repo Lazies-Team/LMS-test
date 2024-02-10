19df9ef5a75d1a04e5a73a6bf88b615da55d9326
berdikulov-571 <mygithub571@gmail.com>
2024-02-11 00:10:11 +0500

    CourseCreationDto

---
+++ src/Application/DataTransferObjects/Courses/CourseCreationDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.DataTransferObjects.Courses
+{
+    public record class CourseCreationDTO
+    (
+        string Name,
+        long SpecialityId,
+        TimeOnly LessonStartAt,
+        TimeOnly LessonEndAt,
+        DateOnly StartAt,
+        DateOnly EndAt
+    );
+}
