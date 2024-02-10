2e5836f7a7b0810f70d6c865ae5533e9d492a3ae
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-02-10 13:49:28 +0500

    homeworks relations implements

---
+++ src/Domain/Entities/Homeworks/Homework.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Comman;
+using Domain.Entities.Lessons;
+using Domain.Entities.Tasks;
+using Domain.Entities.Users;
+
+namespace Domain.Entities.Homeworks
+{
+    public class Homework : Auditable
+    {
+        public string Description { get; set; }
+        public long LessonId { get; set; }
+        public DateTime Deadline { get; set; }
+
+        public Lesson Lesson { get; set; }
+        public ICollection<TaskResult> TaskResults { get; set; }
+        public ICollection<HomeworkFile> HomeworkFiles { get; set; }
+    }
+}
