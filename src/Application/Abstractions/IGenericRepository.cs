cbe5acea6ff104eeaab26a026109f042615dfefd
SardorSohinazarov <sardorstudent0618gmail.com>
2024-02-11 00:56:09 +0500

    Generic repository edited

--- src/Application/Abstractions/IGenericRepository.cs
+++ src/Application/Abstractions/IGenericRepository.cs
@@ -3,7 +3,7 @@
     public interface IGenericRepository<TEntity>
     {
         ValueTask<TEntity> InsertAsync(TEntity entity);
-        IQueryable<IList<TEntity>> SelectAll();
+        IQueryable<TEntity> SelectAll();
         ValueTask<TEntity> SelectByIdAsync(long id);
         ValueTask<TEntity> UpdateAsync(TEntity entity);
         ValueTask<TEntity> DeleteAsync(long id);
