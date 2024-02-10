732893b6423169de60c6b57b48f3d8531a47b192
berdikulov-571 <mygithub571@gmail.com>
2024-02-11 00:50:15 +0500

    TaskResultCreationDTO

---
+++ src/Application/DataTransferObjects/Tasks/TaskResultCreationDTO.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Enums;
+
+namespace Application.DataTransferObjects.Tasks
+{
+    public record class TaskResultCreationDTO
+    (
+        long StudentId,
+        long HomeworkId,
+        string Description,
+        TaskResultStatus TaskResultStatus
+    );
+}
