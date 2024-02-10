426042c3f4b072e2c4e686c2c59c918abde135dc
berdikulov-571 <mygithub571@gmail.com>
2024-02-11 00:50:25 +0500

    StudentModificationDTO

---
+++ src/Application/DataTransferObjects/Users/StudentModificationDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.DataTransferObjects.Users
+{
+    public record class StudentModificationDTO
+    (
+        long Id,
+        long StudentKey,
+        long UserId
+    );
+}
