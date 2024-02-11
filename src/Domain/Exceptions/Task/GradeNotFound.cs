6fd0c698677e62457437a6e76892f04df9937858
berdikulov-571 <mygithub571@gmail.com>
2024-02-11 20:54:47 +0500

    GradeNotFound

---
+++ src/Domain/Exceptions/Task/GradeNotFound.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Exceptions.Task
+{
+    public class GradeNotFound : GlobalException
+    {
+        public GradeNotFound()
+        {
+            StatusCode = System.Net.HttpStatusCode.NotFound;
+            TitleMessage = "Grade Not Found";
+        }
+    }
+}
