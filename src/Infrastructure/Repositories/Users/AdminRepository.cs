58bab1f1c6f383af477b9f3fbf2f9ae58e426bfa
Sardor <sardorstudent0618@gmail.com>
2024-02-11 19:44:11 +0500

    Update AdminRepository.cs

--- src/Infrastructure/Repositories/Users/AdminRepository.cs
+++ src/Infrastructure/Repositories/Users/AdminRepository.cs
@@ -11,9 +11,7 @@ namespace Infrastructure.Repositories.Users
         private readonly ApplicationDbContext _context;
 
         public AdminRepository(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+            => _context = context;
 
         public async ValueTask<Admin> DeleteAsync(long id)
         {
