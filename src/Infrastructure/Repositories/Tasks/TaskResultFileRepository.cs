97e50808151333dbc075f3f85b5f8b1e8c89534c
Sardor <sardorstudent0618@gmail.com>
2024-02-11 23:40:18 +0500

    Update TaskResultFileRepository.cs

--- src/Infrastructure/Repositories/Tasks/TaskResultFileRepository.cs
+++ src/Infrastructure/Repositories/Tasks/TaskResultFileRepository.cs
@@ -11,9 +11,7 @@ namespace Infrastructure.Repositories.Tasks
         private readonly ApplicationDbContext _context;
 
         public TaskResultFileRepository(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+            => _context = context;
 
         public async ValueTask<TaskResultFile> DeleteAsync(long id)
         {
