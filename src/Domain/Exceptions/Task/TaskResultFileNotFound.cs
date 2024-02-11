7c06738792db9e471d2ea5b7d34d1791ff646b81
berdikulov-571 <mygithub571@gmail.com>
2024-02-11 20:54:39 +0500

    TaskResultFileNotFound

---
+++ src/Domain/Exceptions/Task/TaskResultFileNotFound.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Exceptions.Task
+{
+    public class TaskResultFileNotFound : GlobalException
+    {
+        public TaskResultFileNotFound()
+        {
+            StatusCode = System.Net.HttpStatusCode.NotFound;
+            TitleMessage = "Task Result File Not Found !";
+        }
+    }
+}
