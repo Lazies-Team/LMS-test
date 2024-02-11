a6612103863955f91ba655825f10ab551a9a7dfb
berdikulov-571 <mygithub571@gmail.com>
2024-02-11 19:24:37 +0500

    Exception

---
+++ src/Domain/Exceptions/Lesson/AttendanceNotFound.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Exceptions.Lesson
+{
+    public class AttendanceNotFound : GlobalException
+    {
+        public AttendanceNotFound()
+        {
+            StatusCode = System.Net.HttpStatusCode.NotFound;
+            TitleMessage = "Attendance Not Found !";
+        }
+    }
+}
