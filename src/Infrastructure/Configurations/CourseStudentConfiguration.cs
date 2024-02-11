f90ff748f7c5d44cba3b3a14198e916d6fe6c3b0
SardorSohinazarov <sardorstudent0618gmail.com>
2024-02-12 02:01:15 +0500

    Composite key Configurations

---
+++ src/Infrastructure/Configurations/CourseStudentConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations
+{
+    public class CourseStudentConfiguration : IEntityTypeConfiguration<CourseStudent>
+    {
+        public void Configure(EntityTypeBuilder<CourseStudent> builder)
+        {
+            builder.HasKey(x => new { x.StudentId, x.CourseId });
+        }
+    }
+}
---
+++ src/Infrastructure/Configurations/CourseTeacherConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations
+{
+    public class CourseTeacherConfiguration : IEntityTypeConfiguration<CourseTeacher>
+    {
+        public void Configure(EntityTypeBuilder<CourseTeacher> builder)
+        {
+            builder.HasKey(x => new { x.TeacherId, x.CourseId });
+        }
+    }
+}
