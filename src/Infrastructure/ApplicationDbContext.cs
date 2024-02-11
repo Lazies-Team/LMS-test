6cf838dde4e20598038e72568f3bd742687e8d86
berdikulov-571 <mygithub571@gmail.com>
2024-02-11 19:25:39 +0500

    App Db Context

--- src/Infrastructure/ApplicationDbContext.cs
+++ src/Infrastructure/ApplicationDbContext.cs
@@ -1,4 +1,7 @@
-﻿using Domain.Entities.Users;
+﻿using Domain.Entities.Courses;
+using Domain.Entities.Homeworks;
+using Domain.Entities.Lessons;
+using Domain.Entities.Users;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure
@@ -12,5 +15,13 @@ namespace Infrastructure
         public DbSet<User> Users { get; set; }
         public DbSet<Teacher> Teachers { get; set; }
         public DbSet<Student> Students { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Course> Course { get; set; }
+        public DbSet<Specialty> Specialties { get; set; }
+        public DbSet<Homework> Homeworks { get; set; }
+        public DbSet<HomeworkFile> HomeworkFiles { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
+        public DbSet<Lesson> Lessons { get; set; }
     }
 }
