c6e69507ccc21d60837065f68cfc4c4d56c5b925
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-02-12 12:29:52 +0500

    udpated user interface

---
+++ src/Application/Services/Contracts/Users/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using Application.DataTransferObjects.Users;
+using Application.ViewModel;
+
+namespace Application.Services.Contracts.Users
+{
+    public interface IUserService
+    {
+        ValueTask<UserViewModel> AddAsync(UserCreationDTO userCreationDTO);
+        ValueTask<List<UserViewModel>> GetAllAsync();
+        ValueTask<UserViewModel> GetByIdAsync(long id);
+        ValueTask<UserViewModel> UpdateAsync(UserModificationDTO userModificationDTO, long id);
+        ValueTask<UserViewModel> DeleteAsync(long id);
+    }
+}
