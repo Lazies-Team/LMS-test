03caf349abe5c9c1c75b226f63dc52d45ad7eed9
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2024-02-12 12:29:39 +0500

    udpated role interface

---
+++ src/Application/Services/Contracts/Users/IRoleService.cs
@@ -0,0 +1,14 @@
+﻿using Application.DataTransferObjects.Users;
+using Application.ViewModel;
+
+namespace Application.Services.Contracts.Users
+{
+    public interface IRoleService
+    {
+        ValueTask<RoleViewModel> AddAsync(RoleCreateionDTO roleCreateionDTO);
+        ValueTask<List<RoleViewModel>> GetAllAsync();
+        ValueTask<RoleViewModel> GetByIdAsync(long id);
+        ValueTask<RoleViewModel> UpdateAsync(RoleModificationDTO roleModificationDTO, long id);
+        ValueTask<RoleViewModel> DeleteAsync(long id);
+    }
+}
