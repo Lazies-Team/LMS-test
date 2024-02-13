7a22f3da32079f6187090b467f40f30163735b3f
SardorSohinazarov <sardorstudent0618gmail.com>
2024-02-14 01:30:40 +0500

    Admin Service implemted

---
+++ src/Application/Services/Contracts/Users/IAdminService.cs
@@ -0,0 +1,13 @@
+﻿using Application.DataTransferObjects.Users;
+using Application.ViewModel;
+
+namespace Application.Services.Contracts.Users
+{
+    public interface IAdminService
+    {
+        ValueTask<List<AdminViewModel>> GetAllAsync();
+        ValueTask<AdminViewModel> GetByIdAsync(long id);
+        ValueTask<AdminViewModel> UpdateAsync(AdminModificationDTO adminModificationDTO, long id);
+        ValueTask<AdminViewModel> DeleteAsync(long id);
+    }
+}
