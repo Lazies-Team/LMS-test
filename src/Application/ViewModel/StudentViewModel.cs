caeda39d901812f3a0eb063a0b2a6027c6779b6d
SardorSohinazarov <sardorstudent0618gmail.com>
2024-02-15 01:56:45 +0500

    Course and Student viewmodel added

---
+++ src/Application/ViewModel/StudentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities.Courses;
+using Domain.Entities.Tasks;
+
+namespace Application.ViewModel
+{
+    public record StudentViewModel(
+        long Id,
+        string StudentKey,
+        long UserId,
+        //User User,
+        ICollection<Course> Courses,
+        ICollection<TaskResult> TaskResults,
+        DateTime CreatedAt,
+        DateTime UpdatedDate
+    );
+}
