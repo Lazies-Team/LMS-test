dbf192b5eb08231d004ca558b6e21a34b37be3d0
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-02-19 15:48:05 +0500

    IHomeworkService Created

---
+++ src/Application/Services/Contracts/Homeworks/IHomeworkService.cs
@@ -0,0 +1,15 @@
+﻿using Application.DataTransferObjects.Courses;
+using Application.DataTransferObjects.HomeWorks;
+using Application.ViewModel;
+
+namespace Application.Services.Contracts.Homeworks
+{
+    public interface IHomeworkService
+    {
+        ValueTask<HomeworkViewModel> AddAsync(HomeworkCreationDTO homeworkCreationDTO);
+        ValueTask<List<HomeworkViewModel>> GetAllAsync();
+        ValueTask<HomeworkViewModel> GetByIdAsync(long id);
+        ValueTask<HomeworkViewModel> UpdateAsync(HomeworkModificationDTO homeworkModificationDTO, long id);
+        ValueTask<HomeworkViewModel> DeleteAsync(long id);
+    }
+}
