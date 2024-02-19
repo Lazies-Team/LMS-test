8d296b658791b482b0ddaa9f5e31536af4b8bde3
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-02-19 15:48:43 +0500

    HomeworkViewModel Created

---
+++ src/Application/ViewModel/HomeworkViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities.Homeworks;
+using Domain.Entities.Tasks;
+
+namespace Application.ViewModel
+{
+    public record HomeworkViewModel
+    (
+        long Id,
+        string Description,
+        long LessonId,
+        DateTime Deadline,
+        DateTime CreatedAt,
+        DateTime UpdatedDate,
+        ICollection<TaskResult> TaskResults,
+        ICollection<HomeworkFile> HomeworkFiles
+    );
+}
