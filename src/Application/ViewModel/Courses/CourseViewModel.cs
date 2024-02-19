febc49cb870506f0177c41ba7df87766c52c41f8
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-02-19 23:43:24 +0500

    CoursesViewModel Folders

---
+++ src/Application/ViewModel/Courses/CourseViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Application.ViewModel.Lessons;
+using Application.ViewModel.Users;
+using Domain.Entities.Courses;
+
+namespace Application.ViewModel.Courses
+{
+    public record CourseViewModel(
+        long Id,
+        string Name,
+        long SpecialityId,
+        TimeOnly LessonStartAt,
+        TimeOnly LessonEndAt,
+        DateOnly StartAt,
+        DateOnly EndAt,
+
+        Speciality Specialty,
+        ICollection<LessonViewModel> Lessons,
+        ICollection<TeacherViewModel> Teachers,
+        ICollection<StudentViewModel> Students
+    );
+}
---
+++ src/Application/ViewModel/Courses/SpecialityViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities.Courses;
+
+namespace Application.ViewModel.Courses
+{
+    public record SpecialityViewModel
+    (
+        long id,
+        string Name,
+        DateTime CreatedAt,
+        DateTime UpdatedAt,
+        ICollection<Course> Courses
+    );
+
+}
