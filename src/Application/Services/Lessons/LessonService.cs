10359e5b67be79ab55dff740f0ec29b311448b52
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-02-21 01:06:07 +0500

    LessonService Created

---
+++ src/Application/Services/Lessons/LessonService.cs
@@ -0,0 +1,60 @@
+﻿using Application.Abstractions.Lessons;
+using Application.DataTransferObjects.Lessons;
+using Application.Services.Contracts.Lessons;
+using Application.ViewModel.Lessons;
+using Domain.Entities.Lessons;
+using Mapster;
+
+namespace Application.Services.Lessons
+{
+    public class LessonService : ILessonService
+    {
+        private readonly ILessonRepository _lessonRepository;
+
+        public LessonService(ILessonRepository lessonRepository)
+            => _lessonRepository = lessonRepository;
+
+        public async ValueTask<LessonViewModel> AddAsync(LessonCreationDTO lessonCreationDTO)
+        {
+            var lesson = lessonCreationDTO.Adapt<Lesson>();
+            var created = await _lessonRepository.InsertAsync(lesson);
+            var result = created.Adapt<LessonViewModel>();
+
+            return result;
+        }
+
+        public async ValueTask<LessonViewModel> DeleteAsync(long id)
+        {
+            var lesson = await _lessonRepository.DeleteAsync(id);
+            var deleted = lesson.Adapt<LessonViewModel>();
+
+            return deleted;
+        }
+
+        public async ValueTask<List<LessonViewModel>> GetAllAsync()
+        {
+            var lesson = _lessonRepository.SelectAll();
+            var lessons = lesson.ToList().Adapt<List<LessonViewModel>>();
+
+            return lessons;
+        }
+
+        public async ValueTask<LessonViewModel> GetByIdAsync(long id)
+        {
+            var lesson = await _lessonRepository.SelectByIdAsync(id);
+            var result = lesson.Adapt<LessonViewModel>();
+
+            return result;
+        }
+
+        public async ValueTask<LessonViewModel> UpdateAsync(LessonModificationDTO lessonModificationDTO, long id)
+        {
+            var lesson = lessonModificationDTO.Adapt<Lesson>();
+            lesson.Id = id;
+            var update = await _lessonRepository.UpdateAsync(lesson);
+            var result = update.Adapt<LessonViewModel>();
+
+            return result;
+        }
+    }
+}
