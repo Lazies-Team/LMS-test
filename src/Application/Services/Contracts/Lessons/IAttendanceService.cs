480d47e3bfd035f89bd40083c0254a54e69d3fbc
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-02-21 01:33:51 +0500

    IAttendanceService Created

---
+++ src/Application/Services/Contracts/Lessons/IAttendanceService.cs
@@ -0,0 +1,14 @@
+﻿using Application.DataTransferObjects.Lessons;
+using Application.ViewModel.Lessons;
+
+namespace Application.Services.Contracts.Lessons
+{
+    public interface IAttendanceService
+    {
+        ValueTask<AttendanceViewModel> AddAsync(AttendanceCreationDTO attendanceCreationDTO);
+        ValueTask<List<AttendanceViewModel>> GetAllAsync();
+        ValueTask<AttendanceViewModel> GetByIdAsync(long id);
+        ValueTask<AttendanceViewModel> UpdateAsync(AttendanceModificationDTO attendanceModificationDTO, long id);
+        ValueTask<AttendanceViewModel> DeleteAsync(long id);
+    }
+}
