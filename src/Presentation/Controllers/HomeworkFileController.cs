abfa74ef4929e1f3487f7a5f7239c302a5980c22
Sardor <sardorstudent0618@gmail.com>
2024-02-22 23:46:34 +0500

    Update HomeworkFileController.cs

--- src/Presentation/Controllers/HomeworkFileController.cs
+++ src/Presentation/Controllers/HomeworkFileController.cs
@@ -11,48 +11,46 @@ namespace Presentation.Controllers
         private readonly IHomeworkFileService _homeworkFileService;
 
         public HomeworkFileController(IHomeworkFileService homeworkFileService)
-        {
-            _homeworkFileService = homeworkFileService;
-        }
+            => _homeworkFileService = homeworkFileService;
 
         [HttpPost]
         public async Task<IActionResult> PostHomeworkFileAsync([FromForm] HomeworkFileCreationDTO homeworkFileCreationDTO)
         {
-            var created = await _homeworkFileService.AddAsync(homeworkFileCreationDTO);
+            var homeworkFile = await _homeworkFileService.AddAsync(homeworkFileCreationDTO);
 
-            return Ok(created);
+            return Ok(homeworkFile);
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAllHomeworkFileAsync()
         {
-            var getallhomeworkfile = await _homeworkFileService.GetAllAsync();
+            var homeworkFiles = await _homeworkFileService.GetAllAsync();
 
-            return Ok(getallhomeworkfile);
+            return Ok(homeworkFiles);
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetByIdHomeworkFileAsync(long id)
         {
-            var getbyidHomeworkfile = await _homeworkFileService.GetByIdAsync(id);
+            var homeworkFile = await _homeworkFileService.GetByIdAsync(id);
 
-            return Ok(getbyidHomeworkfile);
+            return Ok(homeworkFile);
         }
 
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateHomeworkFileAsync(HomeworkFileModificationDTO homeworkFileModificationDTO, long id)
         {
-            var updatehomeworkfile = await _homeworkFileService.UpdateAsync(homeworkFileModificationDTO, id);
+            var homeworkFile = await _homeworkFileService.UpdateAsync(homeworkFileModificationDTO, id);
 
-            return Ok(updatehomeworkfile);
+            return Ok(homeworkFile);
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteHomeworkFileAsync(long id)
         {
-            var deleted = await _homeworkFileService.DeleteAsync(id);
+            var homeworkFile = await _homeworkFileService.DeleteAsync(id);
 
-            return Ok(deleted);
+            return Ok(homeworkFile);
         }
     }
 }
